5ff3ad133aae2f57017e9d4501cfb73525458746
Ake Hedman <akhe@grodansparadis.com>
2014-09-23 08:37:31 +0200

    Last fixes before merg

--- src/vscp/common/vscphelper.cpp
+++ src/vscp/common/vscphelper.cpp
@@ -1926,7 +1926,6 @@ bool vscp_getVscpDataFromString(vscpEvent *pEvent, const wxString& str)
 	// Check pointers
 	if (NULL == pEvent) return false;
 
-	//wxStringTokenizer tkz( str, _(", \r\n\t") );
 	wxStringTokenizer tkz(str, _(","));
 
 	uint8_t data[ VSCP_MAX_DATA ];
