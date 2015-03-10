61fb7acfb2fe67029a50664eb0749370ccfd720c
Ake Hedman, Paradise of the Frog <akhe@grodansparadis.com>
2015-03-10 12:46:28 +0100

    Added blue-andys changes to the distribution samples

--- src/vscp/common/version.h
+++ src/vscp/common/version.h
@@ -29,9 +29,9 @@
 #define VSCPD_SUB_VERSION   	0
 
 
-#define VSCPD_SUB_SUB_VERSION   87
+#define VSCPD_SUB_SUB_VERSION   88
 
-#define VSCPD_DISPLAY_VERSION "1.0.0.87 Neon"
+#define VSCPD_DISPLAY_VERSION "1.0.0.88 Neon"
 
 #define VSCPD_COPYRIGHT "Copyright (C) 2000-2015, Grodans Paradis AB, http://www.paradiseofthefrog.com"
 #define VSCPD_COPYRIGHT_HTML "Copyright (C) 2000-2015, <a href=\"mailto:info@paradiseofthefrog.com\">Grodans Paradis AB</a>, <a href=\"http://www.paradiseofthefrog.com\">http://www.paradiseofthefrog.com</a>"
