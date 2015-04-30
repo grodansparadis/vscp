d6128b5801ba720953b715e8ca990439630af30d
BlueAndi <web@blue-andi.de>
2015-04-30 20:45:57 +0200

    Success returned, although a error happened.

--- src/vscp/common/vscpremotetcpif.cpp
+++ src/vscp/common/vscpremotetcpif.cpp
@@ -1536,7 +1536,7 @@ int VscpRemoteTcpIf::doCmdVersion( uint8_t *pMajorVer,
     ns_send( m_pClientTcpIpWorkerThread->m_mgrTcpIpConnection.active_connections,
                 strCmd.mb_str(), 
                 strCmd.length() );
-    if ( !checkReturnValue(true) ) return 0;
+    if ( !checkReturnValue(true) ) return VSCP_ERROR_GENERIC;
 
     if ( m_inputStrArray.Count() < 2 ) return VSCP_ERROR_ERROR;   
     m_mutexArray.Lock();
