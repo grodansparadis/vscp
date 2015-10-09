1a004c94d8401a1dfed82662c39d82495e26df45
Ake Hedman, Paradise of the Frog <akhe@grodansparadis.com>
2015-10-09 15:12:31 +0200

    Bumped version

--- src/vscp/common/version.h
+++ src/vscp/common/version.h
@@ -29,9 +29,9 @@
 #define VSCPD_SUB_VERSION   	1
 
 
-#define VSCPD_SUB_SUB_VERSION   72
+#define VSCPD_SUB_SUB_VERSION   73
 
-#define VSCPD_DISPLAY_VERSION "1.0.1.72 Neon"
+#define VSCPD_DISPLAY_VERSION "1.0.1.73 Neon"
 
 #define VSCPD_COPYRIGHT "Copyright (C) 2000-2015, Paradise of the Frog AB, http://www.paradiseofthefrog.com"
 #define VSCPD_COPYRIGHT_HTML "Copyright (C) 2000-2015, <a href=\"mailto:info@paradiseofthefrog.com\">Paradise of the Frog</a>, <a href=\"http://www.paradiseofthefrog.com\">http://www.paradiseofthefrog.com</a>"
