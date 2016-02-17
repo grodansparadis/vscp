c6af62bce32cb92f75ba1c1104546901d71e6064
Ake Hedman <akhe@grodansparadis.com>
2016-02-17 17:08:46 +0100

    Fixed comment error

--- src/vscp/common/tables.h
+++ src/vscp/common/tables.h
@@ -55,17 +55,17 @@ enum  {
 
 // This structure is located at the start of the main file.
 struct _vscpFileHead {
-    uint8_t id[2];              // File id (main: 0x55,0xaa  index 0xaa,0x55
-    uint8_t type;               // VSCP_TABLE_NORMAL/VSCP_TABLE_STATIC
-    char nameTable[64];         // Name of table - Used to reference it
-    char descriptionTable[512];	// Description of table
-    char nameXLabel[128];       // Label for X-axis
-    char nameYLabel[128];       // Label for Y-axis
-    uint16_t vscp_class;        // Should be 10 but in future can be 11/12/13...
-    uint16_t vscp_type;         // Measurement type: temp, current etc
-    uint8_t vscp_unit;          // Measurement unit: e.g. 
-                                // Celsius(1)/Fahrenheit(2)/Kelvin(0) 
-                                && for temperature class=10/Type=6
+    uint8_t id[2];                      // File id (main: 0x55,0xaa  index 0xaa,0x55
+    uint8_t type;                       // VSCP_TABLE_NORMAL/VSCP_TABLE_STATIC
+    char nameTable[64];                 // Name of table - Used to reference it
+    char descriptionTable[512];         // Description of table
+    char nameXLabel[128];               // Label for X-axis
+    char nameYLabel[128];               // Label for Y-axis
+    uint16_t vscp_class;                // Should be 10 but in future can be 11/12/13...
+    uint16_t vscp_type;                 // Measurement type: temp, current etc
+    uint8_t vscp_unit;                  // Measurement unit: e.g. 
+                                        // Celsius(1)/Fahrenheit(2)/Kelvin(0) 
+                                        // && for temperature class=10/Type=6
 
     long unsigned int dummy;            // Next read position for static file   
                                         // !!! NOT USED !!!
