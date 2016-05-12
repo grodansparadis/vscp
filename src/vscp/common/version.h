0e8d7568fcec74749cdfbb4ca0ea9686bb221d2e
Ake Hedman <akhe@grodansparadis.com>
2016-05-12 20:39:16 +0200

    bumped version

--- src/vscp/common/version.h
+++ src/vscp/common/version.h
@@ -27,9 +27,9 @@
 #define VSCPD_MAJOR_VERSION     1
 #define VSCPD_MINOR_VERSION     12
 #define VSCPD_SUB_VERSION       3
-#define VSCPD_BUILD_VERSION     12
+#define VSCPD_BUILD_VERSION     14
 
-#define VSCPD_DISPLAY_VERSION "1.12.3.12 Magnesium"
+#define VSCPD_DISPLAY_VERSION "1.12.3.14 Magnesium"
 
 #define VSCPD_COPYRIGHT "Copyright (C) 2000-2016, Grodans Paradis AB, http://www.paradiseofthefrog.com"
 #define VSCPD_COPYRIGHT_HTML "Copyright (C) 2000-2016, <a href=\"mailto:info@paradiseofthefrog.com\">Paradise of the Frog</a>, <a href=\"http://www.paradiseofthefrog.com\">http://www.paradiseofthefrog.com</a>"
