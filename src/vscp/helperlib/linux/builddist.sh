14e06ac9f0f1ddb31a882b44e4a589b892f3467f
Ake Hedman <akhe@grodansparadis.com>
2017-07-14 10:10:31 +0200

    Version numbering schema changed Initial version of vscphelper lib dist builder

---
+++ src/vscp/helperlib/linux/builddist.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+checkinstall --type debian --pkgname libvscphelper --pkgversion 12.29.0 --arch amd64 --pkrelease 1 --pkglicense MIT --pkggroup developer --pkgsource . --maintainer akhe@grodansparadis.com --requires libwxbase3.0-dev  libssl-dev libcurl4-openssl-dev
