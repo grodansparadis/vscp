ef65e99863a3c5e007875a65de1d8ea905904e69
Ake Hedman <akhe@grodansparadis.com>
2017-07-14 10:28:18 +0200

    Fixed typo

--- src/vscp/helperlib/linux/builddist.sh
+++ src/vscp/helperlib/linux/builddist.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-checkinstall --type debian --pkgname libvscphelper --pkgversion 12.29.0 --arch amd64 --pkrelease 1 --pkglicense MIT --pkggroup developer --pkgsource . --maintainer akhe@grodansparadis.com --requires libwxbase3.0-dev  libssl-dev libcurl4-openssl-dev
+checkinstall --type debian --pkgname libvscphelper --pkgversion 12.29.0 --pkgrelease 1 --pkglicense MIT --pkggroup developer --pkgsource . --maintainer akhe@grodansparadis.com --requires libwxbase3.0-dev,libssl-dev,libcurl4-openssl-dev
