b03af6213e0d5ffc6c1027a2e8f5a03d5a6ae437
Ake Hedman <akhe@grodansparadis.com>
2017-07-14 12:14:06 +0200

    vscphelperlib header is now installed in the correct place during install

--- src/vscp/helperlib/linux/builddist.sh
+++ src/vscp/helperlib/linux/builddist.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-checkinstall --type debian --pkgname libvscphelper --pkgversion 12.29.0 --pkgrelease 1 --pkglicense MIT --pkggroup developer --pkgsource . --maintainer akhe@grodansparadis.com --requires libwxbase3.0-dev,libssl-dev,libcurl4-openssl-dev
+checkinstall --type="debian" --pkgname="libvscphelper-dev" --pkgversion="12.29.0" --pkgrelease="1" --pkglicense="MIT" --pkggroup="developer" --pkgsource="." --maintainer="akhe@grodansparadis.com" --requires="libwxbase3.0-dev \(\>=3.0.0\) ,libssl-dev"
