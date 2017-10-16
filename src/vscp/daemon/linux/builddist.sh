9664f5cb025648842fc8a41e5ba3eb007abce2e3
Ake Hedman <akhe@grodansparadis.com>
2017-10-16 12:31:18 +0200

    Added credits

--- src/vscp/daemon/linux/builddist.sh
+++ src/vscp/daemon/linux/builddist.sh
@@ -7,10 +7,11 @@
 MAJOR_VERSION=`sed '35!d' ../../common/version.h  | cut -b 33-`
 MINOR_VERSION=`sed '36!d' ../../common/version.h  | cut -b 33-`
 RELEASE_VERSION=`sed '37!d' ../../common/version.h  | cut -b 33-`
+BUILD_VERSION=`sed '38!d' ../../common/version.h  | cut -b 33-`
 LIB_PLUS_VER="libvscphelper.so.$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION"
 
 #Build debian
-checkinstall --type="debian" --pkgname="vscpd" --pkgversion="$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION" --pkgrelease="1" --pkglicense="GPL2" --pkggroup="developer" --pkgsource="https://github.com/grodansparadis/vscp" --maintainer="akhe@grodansparadis.com" --requires="libwxbase3.0-dev \(\>=3.0.0\) ,libssl-dev"
+checkinstall --type="debian" --pkgname="vscpd" --pkgversion="$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION" --pkgrelease="$BUILD_VERSION" --pkglicense="GPL2" --pkggroup="developer" --pkgsource="https://github.com/grodansparadis/vscp" --maintainer="akhe@grodansparadis.com" --requires="libwxbase3.0-dev \(\>=3.0.0\) ,libssl-dev"
 
 #build rpm
 checkinstall --type="rpm" --pkgname="libvscphelper-dev" --pkgversion="$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION" --pkgrelease="1" --pkglicense="MIT" --pkggroup="developer" --pkgsource="https://github.com/grodansparadis/vscp" --maintainer="akhe@grodansparadis.com" --requires="libwxbase3.0-dev \(\>=3.0.0\) ,libssl-dev"
