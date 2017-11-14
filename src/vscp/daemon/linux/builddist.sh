1c612478b367098de7b8654c2ba0516b9e55db15
Ake Hedman <akhe@grodansparadis.com>
2017-11-14 18:57:30 +0100

    Fixed deb build script for vscpd

--- src/vscp/daemon/linux/builddist.sh
+++ src/vscp/daemon/linux/builddist.sh
@@ -23,7 +23,7 @@ checkinstall --type="debian" \
 --gzman=yes \
 --pkgsource="https://github.com/grodansparadis/vscp" \
 --maintainer="akhe@grodansparadis.com" \
---requires="libwxbase3.0-dev \(\>=3.0.0\) | libwxgtk3.0-dev \(\>=3.0.0\) ,libssl-dev"
+--requires="libwxbase3.0-dev \(\>=3.0.0\) \| libwxgtk3.0-dev \(\>=3.0.0\) ,libssl-dev"
 
 #build rpm
 #checkinstall --type="rpm" --pkgname="vscpd" --pkgversion="$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION" --pkgrelease="1" --pkglicense="MIT" --pkggroup="developer" --pkgsource="https://github.com/grodansparadis/vscp" --maintainer="akhe@grodansparadis.com" --requires="libwxbase3.0-dev \(\>=3.0.0\) ,libssl-dev"
