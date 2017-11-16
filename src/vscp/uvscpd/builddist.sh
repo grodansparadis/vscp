2562d3a96b3ff40da1035d5e47c6ae0ad74b6104
Ake Hedman <akhe@grodansparadis.com>
2017-11-16 12:19:56 +0100

    Added automatic debian package build scripts

--- src/vscp/uvscpd/builddist.sh
+++ src/vscp/uvscpd/builddist.sh
@@ -4,15 +4,16 @@
 # sudo apt-get install build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev checkinstall
 
 # get version from the version file
-MAJOR_VERSION=`sed '35!d' ../../common/version.h  | cut -b 33-`
-MINOR_VERSION=`sed '36!d' ../../common/version.h  | cut -b 33-`
-RELEASE_VERSION=`sed '37!d' ../../common/version.h  | cut -b 33-`
-BUILD_VERSION=`sed '38!d' ../../common/version.h  | cut -b 33-`
+MAJOR_VERSION=`sed '35!d' ../common/version.h  | cut -b 33-`
+MINOR_VERSION=`sed '36!d' ../common/version.h  | cut -b 33-`
+RELEASE_VERSION=`sed '37!d' ../common/version.h  | cut -b 33-`
+BUILD_VERSION=`sed '38!d' ../common/version.h  | cut -b 33-`
 LIB_PLUS_VER="libvscphelper.so.$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION"
 
 #Build debian
 checkinstall --type="debian" \
---pkgname="vscpd" \
+--pkgname="uvscpd" \
+--install=no \
 --pkgversion="$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION" \
 --pkgrelease="$BUILD_VERSION" \
 --pkglicense="GPL2" \
--- src/vscp/vscpworks/builddist.sh
+++ src/vscp/vscpworks/builddist.sh
@@ -4,15 +4,16 @@
 # sudo apt-get install build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev checkinstall
 
 # get version from the version file
-MAJOR_VERSION=`sed '35!d' ../../common/version.h  | cut -b 33-`
-MINOR_VERSION=`sed '36!d' ../../common/version.h  | cut -b 33-`
-RELEASE_VERSION=`sed '37!d' ../../common/version.h  | cut -b 33-`
-BUILD_VERSION=`sed '38!d' ../../common/version.h  | cut -b 33-`
+MAJOR_VERSION=`sed '35!d' ../common/version.h  | cut -b 33-`
+MINOR_VERSION=`sed '36!d' ../common/version.h  | cut -b 33-`
+RELEASE_VERSION=`sed '37!d' ../common/version.h  | cut -b 33-`
+BUILD_VERSION=`sed '38!d' ../common/version.h  | cut -b 33-`
 LIB_PLUS_VER="libvscphelper.so.$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION"
 
 #Build debian
 checkinstall --type="debian" \
---pkgname="vscpd" \
+--pkgname="vscpworks" \
+--install=no \
 --pkgversion="$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION" \
 --pkgrelease="$BUILD_VERSION" \
 --pkglicense="GPL2" \
@@ -23,7 +24,7 @@ checkinstall --type="debian" \
 --gzman=yes \
 --pkgsource="https://github.com/grodansparadis/vscp" \
 --maintainer="akhe@grodansparadis.com" \
---requires="libwxbase3.0-dev \(\>=3.0.0\) \| libwxgtk3.0-dev \(\>=3.0.0\) ,libssl-dev"
+--requires="libwxgtk3.0-dev \(\>=3.0.0\) ,libssl-dev"
 
 #build rpm
 #checkinstall --type="rpm" --pkgname="vscpd" --pkgversion="$MAJOR_VERSION.$MINOR_VERSION.$RELEASE_VERSION" --pkgrelease="1" --pkglicense="MIT" --pkggroup="developer" --pkgsource="https://github.com/grodansparadis/vscp" --maintainer="akhe@grodansparadis.com" --requires="libwxbase3.0-dev \(\>=3.0.0\) ,libssl-dev"
