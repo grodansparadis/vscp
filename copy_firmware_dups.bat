8a741987b200181831d5c51020846d35ac0586b3
Ake Hedman, Grodans Paradis AB <akhe@grodansparadis.com>
2018-05-22 14:00:44 +0200

    Updated paths to renamed firmware repro.

--- copy_firmware_dups.bat
+++ copy_firmware_dups.bat
@@ -1,21 +1,21 @@
 REM This file is used to make duplicates of critical files for the 
 REM vscp_firmware repository
 
-copy src\vscp\common\canal.h ..\vscp_firmware\common
-copy src\vscp\common\canal.h C:\Users\grodansparadis\development\vscp_install_win32\files\include
+copy src\vscp\common\canal.h ..\vscp-firmware\common
+copy src\vscp\common\canal.h C:\Users\grodansparadis\development\vscp-install-win32\files\include
 
 copy src\vscp\common\vscp.h ..\vscp_firmware\common
-copy src\vscp\common\vscp.h C:\Users\grodansparadis\development\vscp_install_win32\files\include
+copy src\vscp\common\vscp.h C:\Users\grodansparadis\development\vscp-install-win32\files\include
 
-copy src\vscp\common\vscp_class.h ..\vscp_firmware\common
-copy src\vscp\common\vscp_class.h C:\Users\grodansparadis\development\vscp_install_win32\files\include
+copy src\vscp\common\vscp_class.h ..\vscp-firmware\common
+copy src\vscp\common\vscp_class.h C:\Users\grodansparadis\development\vscp-install-win32\files\include
 
-copy src\vscp\common\vscp_type.h ..\vscp_firmware\common
-copy src\vscp\common\vscp_type.h C:\Users\grodansparadis\development\vscp_install_win32\files\include
+copy src\vscp\common\vscp_type.h ..\vscp-firmware\common
+copy src\vscp\common\vscp_type.h C:\Users\grodansparadis\development\vscp-install-win32\files\include
 
-copy src\vscp\common\vscp_serial.h ..\vscp_firmware\common
-copy src\vscp\common\vscp_serial.h C:\Users\grodansparadis\development\vscp_install_win32\files\include
+copy src\vscp\common\vscp_serial.h ..\vscp-firmware\common
+copy src\vscp\common\vscp_serial.h C:\Users\grodansparadis\development\vscp-install-win32\files\include
 
-copy src\vscp\common\canal_macro.h C:\Users\grodansparadis\development\vscp_install_win32\files\include
-copy src\vscp\common\canaldlldef.h C:\Users\grodansparadis\development\vscp_install_win32\files\include
-copy src\vscp\helperlib\vscphelperlib.h C:\Users\grodansparadis\development\vscp_install_win32\files\include
+copy src\vscp\common\canal_macro.h C:\Users\grodansparadis\development\vscp-install-win32\files\include
+copy src\vscp\common\canaldlldef.h C:\Users\grodansparadis\development\vscp-install-win32\files\include
+copy src\vscp\helperlib\vscphelperlib.h C:\Users\grodansparadis\development\vscp-install-win32\files\include
