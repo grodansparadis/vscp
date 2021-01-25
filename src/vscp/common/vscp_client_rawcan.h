3cb2d6454a4982d53d3c1b34cbd50c6d63fbd52c
Ake <akhe@grodansparadis.com>
2021-01-25 21:06:29 +0100

    Added rawcan client

---
+++ src/vscp/common/vscp_client_rawcan.h
@@ -0,0 +1,161 @@
+// vscp_client_udp.h
+//
+// tcp/ip client communication classes.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version
+// 2 of the License, or (at your option) any later version.
+//
+// This file is part of the VSCP (https://www.vscp.org)
+//
+// Copyright:   © 2007-2021
+// Ake Hedman, Grodans Paradis AB, <akhe@vscp.org>
+//
+// This file is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this file see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+//
+
+#if !defined(VSCPCLIENTUDP_H__INCLUDED_)
+#define VSCPCLIENTUDP_H__INCLUDED_
+
+#include "vscp.h"
+#include "vscp_client_base.h"
+
+class vscpClientUdp : public CVscpClient
+{
+
+public:
+
+    vscpClientUdp();
+    ~vscpClientUdp();
+
+    /*!
+        Connect to remote host
+        @param bPoll If true polling is used.
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int connect(void);
+
+    /*!
+        Disconnect from remote host
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int disconnect(void);
+
+    /*!
+        Check if connected.
+        @return true if connected, false otherwise.
+    */
+    virtual bool isConnected(void);
+
+    /*!
+        Send VSCP event to remote host.
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int send(vscpEvent &ev);
+
+    /*!
+        Send VSCP event to remote host.
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int send(vscpEventEx &ex);
+
+    /*!
+        Receive VSCP event from remote host
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int receive(vscpEvent &ev);
+
+    /*!
+        Receive VSCP event ex from remote host
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int receive(vscpEventEx &ex);
+
+    /*!
+        Set interface filter
+        @param filter VSCP Filter to set.
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int setfilter(vscpEventFilter &filter);
+
+    /*!
+        Get number of events waiting to be received on remote
+        interface
+        @param pcount Pointer to an unsigned integer that get the count of events.
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int getcount(uint16_t *pcount);
+
+    /*!
+        Clear the input queue
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int clear(void);
+
+    /*!
+        Get version from interface
+        @param pmajor Pointer to uint8_t that get major version of interface.
+        @param pminor Pointer to uint8_t that get minor version of interface.
+        @param prelease Pointer to uint8_t that get release version of interface.
+        @param pbuild Pointer to uint8_t that get build version of interface.
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int getversion(uint8_t *pmajor,
+                            uint8_t *pminor,
+                            uint8_t *prelease,
+                            uint8_t *pbuild);
+
+    /*!
+        Get interfaces
+        @param iflist Get a list of available interfaces
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int getinterfaces(std::deque<std::string> &iflist);
+
+    /*!
+        Get capabilities (wcyd) from remote interface
+        @return Return VSCP_ERROR_SUCCESS of OK and error code else.
+    */
+    virtual int getwcyd(uint64_t &wcyd);
+
+    /*!
+        Return a JSON representation of connection
+        @return JSON representation as string
+    */
+    virtual std::string toJSON(void);
+
+    /*!
+        Set member variables from JSON representation of connection
+        @param config JSON representation as string
+        @return True on success, false on failure.
+    */
+    virtual bool fromJSON(const std::string& config);
+
+    /*!
+        Getter/setters for connection timeout
+        Time is in milliseconds
+    */
+    virtual void setConnectionTimeout(uint32_t timeout);
+    virtual uint32_t getConnectionTimeout(void);
+
+    /*!
+        Getter/setters for response timeout
+        Time is in milliseconds
+    */
+    virtual void setResponseTimeout(uint32_t timeout);
+    virtual uint32_t getResponseTimeout(void);
+    
+private:
+
+};
+
+#endif
