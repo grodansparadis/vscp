98b0ac9325ff40df6c714d1e1bce0234c734f7e1
Ake <akhe@grodansparadis.com>
2021-02-21 17:02:52 +0100

    Removed non standard c++ in favor for lambdas

--- src/vscp/common/vscphelper.h
+++ src/vscp/common/vscphelper.h
@@ -368,20 +368,17 @@ extern "C"
     // trim from start (in place)
     static inline void vscp_ltrim(std::string& s)
     {
-        s.erase(s.begin(),
-                std::find_if(s.begin(),
-                             s.end(),
-                             std::not1(std::ptr_fun<int, int>(std::isspace))));
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+            return !std::isspace(ch);
+        }));
     }
 
     // trim from end (in place)
     static inline void vscp_rtrim(std::string& s)
     {
-        s.erase(std::find_if(s.rbegin(),
-                             s.rend(),
-                             std::not1(std::ptr_fun<int, int>(std::isspace)))
-                  .base(),
-                s.end());
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+            return !std::isspace(ch);
+        }).base(), s.end());
     }
 
     // trim from both ends (in place)
