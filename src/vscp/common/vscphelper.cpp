b5a574275302685c0d1ccc1a61a8d00b8d5d365b
akhe@grodansparadis.com <c51d8c7dd76d955b80109d02eb17027915340b5b>
2021-03-17 08:10:13 +0100

    Fixed parameters

--- src/vscp/common/vscphelper.cpp
+++ src/vscp/common/vscphelper.cpp
@@ -980,7 +980,7 @@ int vscp_hostname_to_ip(char *ip, const char *hostname)
 //
 
 int
-vscp_getPortFromInterface(std::string& interface)
+vscp_getPortFromInterface(const std::string& interface)
 {
     int port = 0;
     size_t pos;
