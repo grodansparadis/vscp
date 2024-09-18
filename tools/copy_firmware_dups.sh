d5086da92eba98707de10f476246588eeb2157f3
Ake Hedman <akhe@grodansparadis.com>
2024-09-18 22:52:39 +0200

    Corrected paths

--- tools/copy_firmware_dups.sh
+++ tools/copy_firmware_dups.sh
@@ -8,15 +8,15 @@ cp src/vscp/common/canal.h ../vscp-install-windows/files/include
 cp src/vscp/common/vscp.h ../vscp-firmware/common
 cp src/vscp/common/vscp.h ../vscp-install-windows/files/include
 
-cp src/vscp/common/vscp_class.h ../vscp-firmware/common
-cp src/vscp/common/vscp_class.h ../vscp-install-windows/files/include
+cp src/vscp/common/vscp-class.h ../vscp-firmware/common
+cp src/vscp/common/vscp-class.h ../vscp-install-windows/files/include
 
-cp src/vscp/common/vscp_type.h ../vscp-firmware/common
-cp src/vscp/common/vscp_type.h ../vscp-install-windows/files/include
+cp src/vscp/common/vscp-type.h ../vscp-firmware/common
+cp src/vscp/common/vscp-type.h ../vscp-install-windows/files/include
 
-cp src/vscp/common/vscp_serial.h ../vscp-firmware/common
-cp src/vscp/common/vscp_serial.h ../vscp-install-windows/files/include
+cp src/vscp/common/vscp-serial.h ../vscp-firmware/common
+cp src/vscp/common/vscp-serial.h ../vscp-install-windows/files/include
 
-cp src/vscp/common/canal_macro.h ../vscp-install-windows/files/include
+cp src/vscp/common/canal-macro.h ../vscp-install-windows/files/include
 cp src/vscp/common/canaldlldef.h ../vscp-install-windows/files/include
-cp ~/development/VSCP/vscp-helper-lib/vscphelperlib.h ../vscp-install-windows/files/include
+cp ~/development/VSCP/vscp-helper-lib/src/vscphelperlib.h ../vscp-install-windows/files/include
